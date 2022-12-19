d1df8cde95c197c77ecedcc6a824cbae3ff80090
Gravity-vV <2090508826@qq.com>
2022-12-19 12:54:30 +0800

    '重构数据库'

--- db/migrate/20221125134500_add_credit_column_to_courses.rb
+++
@@ -1,5 +0,0 @@
-class AddCreditColumnToCourses < ActiveRecord::Migration[6.1]
-  def change
-    add_column :courses , :credit ,:float
-  end
-end
--- db/migrate/20221125171617_remove_columns_to_course_table.rb
+++
@@ -1,7 +0,0 @@
-class RemoveColumnsToCourseTable < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :courses ,:cno
-    remove_column :courses ,:start_time
-
-  end
-end
